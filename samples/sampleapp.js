72cf65575082d13925608808ad4d89f65edf9f25
Tyler Haun <frodo03211@gmail.com>
2017-10-30 21:05:17 +0000

    Update implementation

--- samples/sampleapp.js
+++ samples/sampleapp.js
@@ -1,6 +1,17 @@
 //  Build our app module, with a dependency on the new angular module.
 var app = angular.module('sampleapp', ['ngAnimate', 'ngMaterial', 'ngSanitize', 'ngMdIcons', 'currency-component']);
 
-app.controller('SampleController', ['$scope', '$mdMedia', '$rootScope', '$window', '$timeout', function($scope, $mdMedia, $rootScope, $window, $timeout) {
-  var vm = this;
-}]);
+app.run(function($ablCurrencyComponentProvider) {
+        console.log('$ablCurrencyComponentProvider', $ablCurrencyComponentProvider);
+        $ablCurrencyComponentProvider.defaultCurrency = 'usd';
+        $ablCurrencyComponentProvider.uniqueCurrency = 'usd';
+        $ablCurrencyComponentProvider.currencies = [{ name: 'me', sign: '#', position: 'prepend' }];
+    })
+    .controller('SampleController', ['$scope', '$mdMedia', '$rootScope', '$window', '$timeout', '$filter', 'availableCurrencies', '$log', function($scope, $mdMedia, $rootScope, $window, $timeout, $filter, availableCurrencies, $log) {
+        var vm = this;
+        vm.price = 123456789;
+        vm.customPrice = 1234567890;
+        vm.customCurrencies = $filter('currencyFilter');
+        console.log('vm.customCurrencies', availableCurrencies.getAvailableCurrencies());
+        //vm.customCurrency = this.customCurrencies[0].name;
+    }]);
